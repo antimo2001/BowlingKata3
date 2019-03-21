242a5874dfa6b2084b02bf431c856ffd89bdba5d
antimo2001 <antimo2001@gmail.com>
2019-03-21 05:59:09 -0400

    new mongo connection config

--- config/local.js
+++ config/local.js
@@ -10,19 +10,19 @@ const log = {
 }
 
 // Setup the mongodb config
-const mongoCertificateConfig = {};
-
 const mongo = {
   mock: !process.env.NOMONGO,
   connect: {
-    uri: 'TODO mongo uri',
-    database: 'TODO mongo database',
-    certificate: mongoCertificateConfig,
+    uri: 'mongodb://localhost:27017',
+    database: 'bowlingkata3',
+    options: {
+      useNewUrlParser: true
+    }
   }
 }
 
 export default {
-  port: PORT || 3000,
+  port: 3000,
   env: NODE_ENV === 'local' ? 'development' : 'production',
   log: log,
   mongo: mongo,
